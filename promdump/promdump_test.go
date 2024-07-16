ade7ef35ee9094808b90f9b6a54e3152340f3532
Ian Anderson <ianderson@yugabyte.com>
2024-07-16 12:53:00 -0400

    promdump: add a test suite
    This commit adds the first test in the test suite. This white box test tests the behaviour of the getMetricName function, verifying that:
    
    1. An error is returned if neither of exportName and jobName are specified for a metric.
    2. An error is returned if both of exportName and jobName are specified for a metric.
    3. The exportName or jobName is returned if exactly one of those fields is specified.

---
+++ promdump/promdump_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestGetMetricName(t *testing.T) {
+	testMetrics := map[string]*promExport{
+		"emptymetric": {collect: true, changedFromDefault: false, requiresNodePrefix: false},
+		"exporter":    {exportName: "exporter_export", collect: true, changedFromDefault: false, requiresNodePrefix: true},
+		"jobmetric":   {jobName: "jobmetric", collect: true, changedFromDefault: false, requiresNodePrefix: false},
+		"badmetric":   {exportName: "badmetric_export", jobName: "badmetric", collect: true, changedFromDefault: false, requiresNodePrefix: true},
+	}
+
+	testMetric := testMetrics["emptymetric"]
+	// presence of one of exportName or jobName must be enforced
+	metricName, err := getMetricName(testMetric)
+	if err == nil {
+		t.Errorf("getMetricName(%+v) must return an error when neither exportName and jobName are present", testMetric)
+	}
+
+	testMetric = testMetrics["badmetric"]
+	// mutual exclusivity of exportName and jobName must be enforced
+	metricName, err = getMetricName(testMetric)
+	if err == nil {
+		t.Errorf("getMetricName(%+v) must return an error when both exportName and jobName are present", testMetric)
+	}
+
+	testMetric = testMetrics["exporter"]
+	// getMetricName must return the export name for exporter metrics
+	metricName, err = getMetricName(testMetric)
+	if metricName != "exporter_export" {
+		t.Errorf("getMetricName(%+v) must return the name of the export", testMetric)
+	}
+
+	testMetric = testMetrics["jobmetric"]
+	// getMetricName must return the job name for job metrics
+	metricName, err = getMetricName(testMetric)
+	if metricName != "jobmetric" {
+		t.Errorf("getMetricName(%+v) must return the name of the job", testMetric)
+	}
+}
